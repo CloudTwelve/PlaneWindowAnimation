f1ebabed8c90dc890a153dbdc42f0724b7170448
CloudTwelve <94795982+CloudTwelve@users.noreply.github.com>
2024-11-05 23:45:38 -0500

    Update script.js
    Added comment crediting ChatGPT

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+// shout-out to ChatGPT, which helped me here
+// kinda did the work for me, but on the bright side I learned about event listeners
+// and query selectors, which is cool!
+
 const windows = document.querySelectorAll('.window');
 const body = document.body;
 
